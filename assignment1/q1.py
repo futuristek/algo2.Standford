15b7610dd2e310c2ed0be06d7e11c22bdc9361cd
futuristek <ginojay@gmail.com>
2017-08-28 09:24:07 -0400

    assignment 1

---
+++ assignment1/q1.py
@@ -0,0 +1,23 @@
+from operator import itemgetter
+
+def greedy():
+	with open('jobs.txt') as f:
+		f.next()
+		j = []
+		for line in f:
+			w, l = map(int, line.split(' '))
+			j.append((w - l, w, l))
+		j = sorted(j, key = itemgetter(0, 1), reverse = True)
+		s = 0
+		ct = 0
+		for v in j:
+			ct += v[2]
+			s += v[1]*ct
+			
+		return s
+
+def main():
+	print greedy()
+
+if __name__ == '__main__':
+	main()
